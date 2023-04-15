fd1271108a745913a32b8db62bdd67b09cdd0b5f
Keshari, Akash <akash_keshari@optum.com>
2023-04-15 14:59:07 +0530

    change to id in stead of string

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
 const _= require('lodash');
 
-const {GraphQLObjectType, GraphQLString,GraphQLSchema} = graphql;
+const {GraphQLObjectType, GraphQLString,GraphQLSchema, GraphQLID} = graphql;
 
 var books=[
     {name: 'Annhilation of caste', genre:'Essay', id:'1'},
@@ -11,7 +11,7 @@ var books=[
 const BookType = new GraphQLObjectType({
     name:'Book',
     fields:() => ({
-        id:{type: GraphQLString},
+        id:{type: GraphQLID},
         name:{type : GraphQLString},
         genre:{type: GraphQLString}
     })
@@ -22,7 +22,7 @@ const RootQuery = new GraphQLObjectType({
     fields :{
         book:{
             type:BookType,
-            args :{ id :{type: GraphQLString}},
+            args :{ id :{type: GraphQLID}},
             resolve(parent, args){
                 //code to get data from db/ other source
                 //current i am using dummy data from array defined above as var books for this purpose but we can code to fetch data from db too
