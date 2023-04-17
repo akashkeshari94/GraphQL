6169c2ba8e12444acda8720790affdc6838dc9cb
Keshari, Akash <akash_keshari@optum.com>
2023-04-17 16:46:41 +0530

    learning graphql and node 17 April

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,17 +1,11 @@
 const graphql = require('graphql');
-const _= require('lodash');
 
-const {GraphQLObjectType, GraphQLString,GraphQLSchema, GraphQLID} = graphql;
+const {GraphQLObjectType, GraphQLString,GraphQLSchema} = graphql;
 
-var books=[
-    {name: 'Annhilation of caste', genre:'Essay', id:'1'},
-    {name: 'Miracle Morning', genre:'Self-help', id:'2'},
-    {name: '2 States', genre:'Romantice', id:'3'},
-];
 const BookType = new GraphQLObjectType({
     name:'Book',
     fields:() => ({
-        id:{type: GraphQLID},
+        id:{type: GraphQLString},
         name:{type : GraphQLString},
         genre:{type: GraphQLString}
     })
@@ -22,11 +16,9 @@ const RootQuery = new GraphQLObjectType({
     fields :{
         book:{
             type:BookType,
-            args :{ id :{type: GraphQLID}},
+            args :{ id :{type: GraphQLString}},
             resolve(parent, args){
                 //code to get data from db/ other source
-                //current i am using dummy data from array defined above as var books for this purpose but we can code to fetch data from db too
-                return _.find(books,{id: args.id});
             }
         }
     }
@@ -34,4 +26,4 @@ const RootQuery = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
     query:RootQuery
-});
+})
