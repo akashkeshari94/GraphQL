5bf5602adf8ae90ff594acf8ec767f35afaa0475
Keshari, Akash <akash_keshari@optum.com>
2023-04-19 00:37:20 +0530

    Added mutation functionality to insert record and fecth records

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,8 +1,11 @@
 const graphql = require('graphql');
 const _= require('lodash');
+const Book= require('../models/book');
+const Author= require('../models/author');
 
 const {GraphQLObjectType, GraphQLString,GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList} = graphql;
 
+/*
 var books=[
     {name: 'Annhilation of caste', genre:'Essay', id:'1', authorId:'1'},
     {name: 'Miracle Morning', genre:'Self-help', id:'2', authorId:'2'},
@@ -19,6 +22,7 @@ var authors=[
     {name: 'Hal Elrod', age:43, id:'2'},
     {name: 'Chetan', age:56, id:'3'}
 ];
+*/
 
 const BookType = new GraphQLObjectType({
     name:'Book',
@@ -30,7 +34,9 @@ const BookType = new GraphQLObjectType({
             type: AuthorType,
             resolve(parent, args){
                 console.log(parent);
-                return _.find(authors,{id:parent.authorId})
+                //return _.find(authors,{id:parent.authorId})
+                return Author.findById(parent.authorId);
+
             }
         }
 
@@ -48,7 +54,8 @@ const AuthorType = new GraphQLObjectType({
             type: new GraphQLList(BookType),
             resolve(parent, args){
                 console.log(parent);
-                return _.filter(books,{authorId:parent.id})
+                //return _.filter(books,{authorId:parent.id})
+                return Book({authorId:parent.id});
             }
         }
     })
@@ -64,7 +71,8 @@ const RootQuery = new GraphQLObjectType({
                 //code to get data from db/ other source
                 //current i am using dummy data from array defined above as var books for this purpose but we can code to fetch data from db too
                 console.log(typeof(args.id))
-                return _.find(books,{id: args.id});
+                //return _.find(books,{id: args.id});
+                return Book.findById(args.id)
             }
         },
         author:{
@@ -74,26 +82,66 @@ const RootQuery = new GraphQLObjectType({
                 //code to get data from db/ other source
                 //current i am using dummy data from array defined above as var books for this purpose but we can code to fetch data from db too
                 console.log(typeof(args.id))
-                return _.find(authors,{id: args.id});
+                //return _.find(authors,{id: args.id});
+                return Author.findById(args.id);
             }
         },
 
         books:{
             type: new GraphQLList(BookType),
             resolve(){
-                return books;
+                //return books;
+                return Book.find({});
             }
         },
 
         authors:{
             type: new GraphQLList(AuthorType),
             resolve(){
-                return authors;
+                //return authors;
+                return Author.find({});
+            }
+        }
+    }
+});
+
+const Mutation= new GraphQLObjectType({
+    name:'Mutation',
+    fields:{
+        addAuthor:{
+            type:AuthorType,
+            args:{
+                name:{type:GraphQLString},
+                age:{type:GraphQLInt}
+            },
+            resolve(parent, args){
+                let author= new Author({
+                    name:args.name,
+                    age:args.age
+                });
+                return author.save();
+            }
+        },
+        addBook:{
+            type:BookType,
+            args:{
+                name:{type:GraphQLString},
+                genre:{type:GraphQLString},
+                authorId:{type:GraphQLID}
+            },
+            resolve(parent, args){
+                let book = new Book({
+                    name:args.name,
+                    genre:args.genre,
+                    authorId:args.authorId
+                });
+                return book.save();
             }
         }
     }
 });
 
 module.exports = new GraphQLSchema({
-    query:RootQuery
+    query:RootQuery,
+    mutation:Mutation
 });
