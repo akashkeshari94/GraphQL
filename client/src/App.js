50b38d8c20b724dba33333eb72f0c88f3b5bb4d0
Akash Keshari <30547546+akashkeshari94@users.noreply.github.com>
2023-04-22 15:27:04 +0530

    Set up Apollo Client for graphql query

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
+//components
 import BookList from "./components/BookList";
+
+//Apollo Client set Up
+const client= new ApolloClient({
+  uri:'http://localhost:4000/graphql'
+})
 
 function App() {
   return (
-    <div id="main">
+    <ApolloProvider client={client}>
+       <div id="main">
       <h1>Best Reading List</h1>
       <BookList/>
     </div>
+    </ApolloProvider>
+   
   );
 }
 
